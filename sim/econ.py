13d5f2bbbc74b2683383d5cafcf0d33096f3bc5c
Kirk Roerig <mr.possoms@gmail.com>
2022-05-01 07:39:24 -0600

    adding multiple sim methods

--- sim/econ.py
+++ sim/econ.py
@@ -4,43 +4,72 @@ import numpy as np
 import plotly
 import plotly.subplots
 import plotly.graph_objects as go
+import argparse
 
-goods = 6
-stm = np.random.randn(goods, goods)
-print(stm)
-mask = np.random.randint(2, size=stm.shape)
-print(mask)
-stm *= mask
-np.fill_diagonal(stm, -np.abs(np.random.randn(goods)))
+args = argparse.ArgumentParser()
+args.add_argument('--method', action='store', type=int, default=0)
 
-x = np.random.randn(goods, 1)
+args = args.parse_args()
 
+def stms():
+	goods = 6
+	stm = np.random.randn(goods, goods)
+	print(stm)
+	mask = np.random.randint(2, size=stm.shape)
+	print(mask)
+	stm *= mask
+	np.fill_diagonal(stm, -np.abs(np.random.randn(goods)))
 
+	x = np.random.randn(goods, 1)
 
-print(stm)
+	print(stm)
 
-X = [x]
+	X = [x]
 
-traces = {}
+	traces = {}
 
-for _ in range(500):
-	x_t = X[-1]
+	for _ in range(500):
+		x_t = X[-1]
+		for i in range(x.shape[0]):
+			if i not in traces:
+				traces[i] = []
+			else:
+				traces[i].append(x_t[i][0])
+
+		X.append(X[-1] + np.matmul(stm, X[-1]) * 0.1)
+
+	#for i in range(x.shape[0]):
+	#	print(f'{i}: {traces[i]}')
+
+	fig = plotly.subplots.make_subplots(rows=1, cols=1, shared_xaxes=True)
+
 	for i in range(x.shape[0]):
-		if i not in traces:
-			traces[i] = []
-		else:
-			traces[i].append(x_t[i][0])
+		fig.add_trace(
+			go.Scatter(y=traces[i]),
+			row=1, col=1)
 
-	X.append(X[-1] + np.matmul(stm, X[-1]) * 0.1)
+	fig.show()
 
-#for i in range(x.shape[0]):
-#	print(f'{i}: {traces[i]}')
 
-fig = plotly.subplots.make_subplots(rows=1, cols=1, shared_xaxes=True)
+def foob():
 
-for i in range(x.shape[0]):
-	fig.add_trace(
-		go.Scatter(y=traces[i]),
-		row=1, col=1)
+	# I think supply and demand might be 2 sides of the same coin
+	def supply():
+		# todo
+		return
 
-fig.show()
+	def demand():
+		# todo
+		return
+
+	def price(supply, demand):
+		return supply / demand
+
+if __name__ == '__main__':
+
+	methods = {
+		0: stms,
+		1: foob
+	}
+
+	methods[args.method]()
