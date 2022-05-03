939be4581f7c2479fd798bdaf5fa809412dfe16c
Kirk Roerig <mr.possoms@gmail.com>
2022-05-02 18:17:41 -0600

    WIP

--- sim/econ.py
+++ sim/econ.py
@@ -57,11 +57,16 @@ def foob():
 	demand = np.maximum(np.zeros(goods), np.random.randn(goods))
 	
 	inputs = np.abs(np.random.randn(goods, goods))
-	inputs = np.fill_diagonal(inputs, 0)
+	np.fill_diagonal(inputs, 0)
+	print(inputs)
+	inputs = np.tril(inputs)
 
 	D_traces = {}
+	P_traces = {}
+	S_traces = {}
 	D = [demand]
 	S = [np.maximum(np.zeros(goods), np.random.randn(goods))]
+	P = [demand * 0]
 
 	# I think supply and demand might be 2 sides of the same coin
 	def supply():
@@ -72,30 +77,53 @@ def foob():
 	# 	# todo
 	# 	return
 
-	def price(good, demand, inputs):
-
+	def price(good, supply, demand, inputs, profit=1.2, last=None):
+		if last == good:
+			return 0
 		# pi(x, t) = x * p(x) - C(x) - tx
 		# where p(x) is the market price if it is supplied at rate x
 		# C(x) is the production cost
 		# x * p(x) is the revenue returned from selling
 		
+		prod_cost = 0
+		for i in range(len(inputs[good])):
+			prod_cost += price(i, supply, demand, inputs, i)
+
+		sale_price = prod_cost * profit_percentage
+
 		# return supply / demand
-		pass
+		return sale_price * (demand[good] / supply[good])
 
 
 
 	for _ in range(500):
 		D_t_1 = D[-1]
+		S_t_1 = S[-1]
+		P_t_1 = P[-1]
 
 		for i in range(goods):
 			if i not in D_traces:
 				D_traces[i] = []
-			else:
-				D_traces[i].append(D_t_1[i])
-
+				S_traces[i] = []
+				P_traces[i] = []
+			
+			D_traces[i].append(D_t_1[i])
+			S_traces[i].append(S_t_1[i])
+			P_traces[i].append(P_t_1[i])
 
 		# simulate demand as a random walk
 		D.append(np.maximum(0, D_t_1 + np.random.randn(goods)))
+
+		# make supply lag demand simulated as an alpha filter
+		r = D_t_1 - S_t_1
+		a = 0.1
+		S.append(S_t_1 + a * r)
+		
+		p = np.zeros(goods)
+		for i in range(goods):
+			p[i] = price(i, S_t_1, D_t_1, inputs)
+		P.append(p)
+
 
 	fig = plotly.subplots.make_subplots(rows=1, cols=1, shared_xaxes=True)
 
@@ -106,6 +134,15 @@ def foob():
 	for i in range(goods):
 		fig.add_trace(
 			go.Scatter(y=D_traces[i]),
+			# name='demand',
+			row=1, col=1)
+		fig.add_trace(
+			go.Scatter(y=S_traces[i]),
+			# name='supply',
+			row=1, col=1)
+		fig.add_trace(
+			go.Scatter(y=P_traces[i]),
+			# name='price',
 			row=1, col=1)
 
 	fig.show()
